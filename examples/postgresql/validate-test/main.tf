621e552250f23b35ce6d5cb4a40ff635d6a77bdc
Espen Blikra <espen.blikra@entur.org>
2022-10-13 10:45:37 +0200

    change tf validation of postgresql module to example

---
+++ examples/postgresql/validate-test/main.tf
@@ -0,0 +1,23 @@
+provider "azurerm" {
+  features {}
+}
+
+module "postgresql" {
+  source = "github.com/entur/terraform-azure-sql//modules/postgresql?ref=v0.0.8" # Releases: https://github.com/entur/terraform-azure-sql/releases (x-release-please-version)
+  # source = "../../../modules/postgresql"
+
+  resource_group_name = var.resource_group_name
+  location            = var.location
+  tags                = var.tags
+
+  app_name     = var.app_name
+  environment  = var.environment
+  landing_zone = var.landing_zone
+
+  server_version = "11"
+  sku_name       = "GP_Gen5_2"
+  storage_mb     = 5120
+
+  databases      = var.databases
+  database_roles = var.database_roles
+}
---
+++ examples/postgresql/validate-test/variables.tf
@@ -0,0 +1,72 @@
+variable "app_name" {
+  type    = string
+  default = "myapp"
+}
+
+variable "environment" {
+  type    = string
+  default = "myenv"
+}
+
+variable "landing_zone" {
+  type    = string
+  default = "mylandingzone"
+}
+
+variable "location" {
+  type    = string
+  default = "Norway East"
+}
+
+variable "resource_group_name" {
+  type    = string
+  default = "myapprg"
+}
+
+variable "tags" {
+  type    = map(any)
+  default = {}
+}
+
+variable "databases" {
+  type    = list(string)
+  default = ["cats", "dogs"]
+}
+
+variable "database_roles" {
+  type = map(any)
+  default = {
+    application = {
+      name              = "appuser"
+      password_override = null
+      replication       = false
+      roles             = []
+      grants = [
+        {
+          database    = "cats"
+          schema      = "cats"
+          object_type = "database"
+          privileges  = ["CONNECT"]
+        },
+        {
+          database    = "cats"
+          schema      = "cats"
+          object_type = "schema"
+          privileges  = ["USAGE", "CREATE"]
+        },
+        {
+          database    = "dogs"
+          schema      = "dogs"
+          object_type = "database"
+          privileges  = ["CONNECT"]
+        },
+        {
+          database    = "dogs"
+          schema      = "dogs"
+          object_type = "schema"
+          privileges  = ["USAGE", "CREATE"]
+        }
+      ]
+    }
+  }
+}
