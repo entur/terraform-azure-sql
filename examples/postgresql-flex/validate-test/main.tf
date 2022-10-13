512f187b43b52889f46c9861b70054a6cd887778
Espen Blikra <espen.blikra@entur.org>
2022-10-13 09:27:15 +0200

    feat: test terraform validate dir override

---
+++ examples/postgresql-flex/validate-test/main.tf
@@ -0,0 +1,25 @@
+provider "azurerm" {
+  features {}
+}
+
+module "postgresql-flexserver" {
+  #source = "github.com/entur/terraform-azure-sql//modules/postgresql-flex?ref=v0.0.8" # Releases: https://github.com/entur/terraform-azure-sql/releases (x-release-please-version)
+  source = "../../../modules/postgresql-flex"
+
+  resource_group_name = var.resource_group_name
+  location            = var.location
+  tags                = var.tags
+
+  app_name     = var.app_name
+  environment  = var.environment
+  landing_zone = var.landing_zone
+
+  server_version = "12"
+  sku_name       = "GP_Standard_D4s_v3"
+  storage_mb     = 32768
+
+  databases      = var.databases
+  database_roles = var.database_roles
+
+  maintenance_window = var.maintenance_window #You can remove this line if you want to keep the maintenance_window setup as default.
+}
---
+++ examples/postgresql-flex/validate-test/variables.tf
@@ -0,0 +1,68 @@
+variable "app_name" {
+  type    = string
+  default = "validate"
+}
+
+variable "environment" {
+  type    = string
+  default = "sandbox"
+}
+
+variable "landing_zone" {
+  type    = string
+  default = "sandbox-001"
+}
+
+variable "location" {
+  type    = string
+  default = "Norway East"
+}
+variable "resource_group_name" {
+  type    = string
+  default = "validate"
+}
+variable "tags" {
+  type    = map(any)
+  default = {}
+}
+
+variable "databases" {
+  type    = list(string)
+  default = ["cats", "dogs"]
+}
+
+variable "database_roles" {
+  type = map(any)
+  default = {
+    application = {
+      name              = "appuser"
+      password_override = null
+      replication       = false
+      roles             = []
+      grants = [
+        {
+          database    = "cats"
+          schema      = "cats"
+          object_type = "database"
+          privileges  = ["CONNECT"]
+        },
+        {
+          database    = "dogs"
+          schema      = "dogs"
+          object_type = "database"
+          privileges  = ["CONNECT"]
+        }
+      ]
+    }
+  }
+}
+
+variable "maintenance_window" {
+  description = "Configure maintenance window day, start hour and start minute, default is Sunday= '0', hour= '0', minute= '0' "
+  type        = map(string)
+  default     = {
+    day_of_week  = 5
+    start_hour   = 22
+    start_minute = 0
+  }
+}
